fe80b4e3163319159581dbe5b943e94979fcdbca
Cory Gray <crgray21@yahoo.com>
2018-04-23 03:48:08 -0400

    Added changes to test for multiple objects in scene

---
+++ Assets/AddSofa.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddSofa : MonoBehaviour {
+
+	public void onSofaClick()
+    {
+        GameObject gameObject = GameObject.Find("Ground Plane Stage");
+        Transform parent = gameObject.transform;
+        parent.GetChild(0).Rotate(0, 180, 0);
+        parent.GetChild(0).localScale.Set((float).001, (float).001, (float).001);
+
+    }
+}
---
+++ Assets/Scripts/AddSofaClick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddSofaClick : MonoBehaviour {
+
+    public void OnSofaClick()
+    {
+        GameObject gameObject = GameObject.Find("Ground Plane Stage");
+        Transform parent = gameObject.transform;
+ //       parent.GetChild(0).gameObject.AddComponent<String>() as String;
+
+
+    }
+}
